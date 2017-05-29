ab7f855a3c53fd5ae95d23be9f581bd2f3c3c889
Hendra Permana <hynra@yahoo.com>
2017-05-29 22:40:01 +0700

    Connection Factory

---
+++ wortel/src/main/java/com/github/hynra/wortel/ConnectionFactory.java
@@ -0,0 +1,84 @@
+package com.github.hynra.wortel;
+
+/**
+ * Created by hynra on 5/29/17.
+ */
+
+public class ConnectionFactory {
+
+    private String excahnge;
+    private String routingKey;
+    private String hostName;
+    private String virtualHostName;
+    private String username;
+    private String password;
+    private int port;
+
+    public ConnectionFactory(String hostName, String virtualHostName, String username, String password, String excahnge, String routingKey, int port) {
+        this.excahnge = excahnge;
+        this.routingKey = routingKey;
+        this.hostName = hostName;
+        this.virtualHostName = virtualHostName;
+        this.username = username;
+        this.password = password;
+        this.port = port;
+    }
+
+    public String getExcahnge() {
+        return excahnge;
+    }
+
+    public void setExcahnge(String excahnge) {
+        this.excahnge = excahnge;
+    }
+
+    public String getRoutingKey() {
+        return routingKey;
+    }
+
+    public void setRoutingKey(String routingKey) {
+        this.routingKey = routingKey;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getVirtualHostName() {
+        return virtualHostName;
+    }
+
+    public void setVirtualHostName(String virtualHostName) {
+        this.virtualHostName = virtualHostName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+
+}
